db662914fffa85573502d81e49376732066f7617
Alexandru Robert Lazar <alex2000robert@yahoo.com>
2020-12-23 09:46:53 +0000

    Kata 7

---
+++ src/kata7/control/BlockPresenter.java
@@ -0,0 +1,32 @@
+package kata7.control;
+
+import kata7.model.Block;
+import kata7.view.BlockDisplay;
+
+public class BlockPresenter implements Block.Observer{
+    private final Block block;
+    private final BlockDisplay blockDisplay;
+
+    public BlockPresenter(Block block, BlockDisplay blockDisplay) {
+        this.block = block;
+        this.blockDisplay = blockDisplay;
+        this.block.register(this);
+        this.blockDisplay.on(new BlockDisplay.Moved() {
+            @Override
+            public void to(int x, int y) {
+                block.movedTo(x / 100 + 1, Block.MAX - y / 100);
+                BlockPresenter.this.refresh();
+            }
+        });
+        this.refresh();
+    }
+
+    @Override
+    public void changed() {
+        this.refresh();
+    }
+
+    private void refresh() {
+        blockDisplay.paintBlock((block.x()-1)*BlockDisplay.SIZE, (Block.MAX-block.y())*BlockDisplay.SIZE);
+    }
+}
